a062f2a36a72cbdc2d10b135b46f0c8eefea8750
XMS_JZhan <xms_jzhan@XMS-JZhan.local>
2019-01-26 16:12:28 -0700

    Example project from BNR iOS Book v4

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by XMS_JZhan on 1/26/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRQuizViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
